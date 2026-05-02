92bad4b15eb1639e3fd716f7f46665aa7d5fe4c6
engalharbis <eng.alharbi.s@gmail.com>
2026-05-02 15:09:36 +0300

    Update PortfolioDashboard.jsx

--- src/PortfolioDashboard.jsx
+++ src/PortfolioDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useStore } from '../context/store'
+import { useStore } from './store'
 import { calculateAll, fmtCurrency, fmtPct, RATINGS, PROPERTY_TYPES } from '../utils/calculator'
 import { EmptyState } from './ui'
 
